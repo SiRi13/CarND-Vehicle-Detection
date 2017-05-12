45f6a6d75c7c8b2be8c621a89429ac300e6dc81f
Simon <simon.ritzel+git@gmail.com>
2017-05-12 10:31:27 +0200

    color classifier

---
+++ exercises/color_classify.py
@@ -0,0 +1,59 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+import norm_shuffle
+import utils
+
+cars, notcars = utils.get_images()
+
+# TODO play with these values to see how your classifier
+# performs under different binning scenarios
+spatial = 32
+histbin = 32
+
+car_features = norm_shuffle.extract_features(cars, cspace='RGB', spatial_size=(spatial, spatial),
+                                hist_bins=histbin, hist_range=(0, 256))
+notcar_features = norm_shuffle.extract_features(notcars, cspace='RGB', spatial_size=(spatial, spatial),
+                                    hist_bins=histbin, hist_range=(0, 256))
+
+# Create an array stack of feature vectors
+X = np.vstack((car_features, notcar_features)).astype(np.float64)
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2,
+                                                    random_state=rand_state)
+
+print('Using spatial binning of:',spatial, 'and', histbin,'histogram bins')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# Check the prediction time for a single sample
+t=time.time()
+n_predict = 10
+print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+print('For these',n_predict, 'labels: ', y_test[0:n_predict])
+t2 = time.time()
+print(round(t2-t, 5), 'Seconds to predict', n_predict,'labels with SVC')
