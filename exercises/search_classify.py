7eba99e2361161561ba5c0c0bc7439f003085074
Simon <simon.ritzel+git@gmail.com>
2017-05-12 15:36:17 +0200

    hog subsample, multi detections & false positives

--- exercises/search_classify.py
+++ exercises/search_classify.py
@@ -105,7 +105,7 @@ pix_per_cell = 8 # HOG pixels per cell
 cell_per_block = 2 # HOG cells per block
 hog_channel = 1 # Can be 0, 1, 2, or "ALL"
 spatial_size = (16, 16) # Spatial binning dimensions
-hist_bins = 16    # Number of histogram bins
+hist_bins = 64    # Number of histogram bins
 spatial_feat = True # Spatial features on or off
 hist_feat = True # Histogram features on or off
 hog_feat = True # HOG features on or off
@@ -186,3 +186,18 @@ window_img = draw_boxes.draw_boxes(draw_image, bboxes, color=(0, 0, 255), thick=
 
 plt.imshow(window_img)
 plt.show()
+
+import pickle
+
+pickle.dump(bboxes, open("bbox_pickle.p", 'wb'))
+
+dist_pickle = dict()
+dist_pickle["svc"] = svc
+dist_pickle["scaler"] = X_scaler
+dist_pickle["orient"] = orient
+dist_pickle["pix_per_cell"] = pix_per_cell
+dist_pickle["cell_per_block"] = cell_per_block
+dist_pickle["spatial_size"] = spatial_size
+dist_pickle["hist_bins"] = hist_bins
+
+pickle.dump(dist_pickle, open('svc_pickle.p', 'wb'))
